96ba5540f3f696fb2b4727248807e587186e2379
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-27 22:37:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+  if block_given?
+    newArray=[]
+    
+    i = 0 
+    
+    while ( i < array.size)
+      newArray.push(yield(array[i]))
+      i = i + 1 
+    end 
+  end 
+  return newArray
+end 
