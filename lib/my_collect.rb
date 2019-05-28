1140524aedd05219fa04ba10ed0ddabd358509e7
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-05-27 22:28:18 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+    i = 0
+    newCollection = []
+    while i < collection.length
+        newCollection.push yield collection[i]
+        i = i + 1
+    end
+    newCollection
+end
+
 
 
